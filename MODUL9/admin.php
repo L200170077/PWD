73cd3c18b9593bddeb4a44d36bff92ebf96f4143
L200170077 <48081194+L200170077@users.noreply.github.com>
2019-12-09 15:25:46 +0700

    Add files via upload

---
+++ MODUL9/admin.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Selamat Datang Admin</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<div class="kotakpageadmin">
+    <br>
+    <center><h1>Welcome Admin</h1></center>
+
+</div>
+<center><button onclick="window.location.href='logout2.php';">LOGOUT</button></center>
+   
+</body>
+</html>
---
+++ MODUL9/member.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Selamat Datang Member</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <div class="kotakpageutama">
+    <br>
+    <center><h1>Welcome Member</h1></center>
+
+</div>
+<center><button onclick="window.location.href='logout2.php';">LOGOUT</button></center>
+</body>
+</html>
